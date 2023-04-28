1d606862221ac76a52b2be73b840b459212b1dd6
ddeblander <david.deblander@condorcet.be>
2023-04-28 11:22:41 +0200

    -add AbstractModel class -modify LecteurModel and RayonModel to be suitable with AbstractModel

--- src/model/LecteurModel.java
+++ src/model/LecteurModel.java
@@ -7,74 +7,19 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LecteurModel implements DAO,SpecialLecteur
+public class LecteurModel extends AbstractModel<Lecteur> implements SpecialLecteur
 {
     private int numcli = 0;
-    private List<Lecteur> lecteurs;
-    public LecteurModel(){
-        lecteurs = new ArrayList<>();
-        populate();
-    }
-    @Override
-    public Lecteur add(Object o)
+
+    public Lecteur add(Lecteur nl)
     {
-        Lecteur l=(Lecteur) o;
-        boolean present = lecteurs.contains(l);
-        if (!present)
+        if(super.add(nl)!=null)
         {
-            numcli++;
-            l.setNumlecteur(numcli);
-            lecteurs.add(l);
-            return l;
-        } else
-            return null;
-    }
-
-    @Override
-    public boolean remove(Object o)
-    {
-        return lecteurs.remove((Lecteur)o);
-
-    }
-
-    @Override
-    public boolean update(Object o)
-    {
-        Lecteur l =(Lecteur) o;
-        int id=lecteurs.indexOf(l);
-        if(id<0)
-        {
-            return false;
-        }
-        lecteurs.set(id,l);
-        return true;
-    }
-
-    @Override
-    public List getAll()
-    {
-        return lecteurs;
-    }
-
-    @Override
-    public Lecteur read(Object o)
-    {
-        if(o == null)
-        {
-            return null;
-        }
-        Lecteur l = (Lecteur) o;
-        for(Lecteur i : lecteurs)
-        {
-            if(i.equals(l))
-            {
-                return i;
-            }
+            nl.setNumlecteur(++numcli);
+            return nl;
         }
         return null;
     }
-
-
     @Override
     public List<Exemplaire> exemplairesEnLocation(Lecteur l)
     {
@@ -86,18 +31,5 @@ public class LecteurModel implements DAO,SpecialLecteur
     {
         return new ArrayList<>(l.listerExemplairesLoues());
     }
-    private void populate()
-    {
-        try
-        {
-            Lecteur lec = new Lecteur(0,"Dupont","Jean", LocalDate.of(2000,1,4),"Mons","jean.dupont@mail.com","0458774411");
-            add(lec);
-            lec = new Lecteur(0,"Durant","Aline",LocalDate.of(1980,10,10),"Binche","aline.durant@mail.com","045874444");
-            add(lec);
-        }catch(Exception e)
-        {
 
-        }
-
-    }
 }
--- src/model/RayonModel.java
+++ src/model/RayonModel.java
@@ -1,59 +1,11 @@
 package model;
 
-import metier.Lecteur;
 import metier.Rayon;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class RayonModel implements DAO<Rayon> {
-
-    private List<Rayon> rayons = new ArrayList<>();
-
-    @Override
-    public boolean remove(Rayon elt)
-    {
-        return rayons.remove(elt);
-    }
-
-    @Override
-    public boolean update(Rayon elt)
-    {
-        int p = rayons.indexOf(elt);
-        if (p < 0) return false;
-        rayons.set(p, elt);
-        return true;
-    }
+public class RayonModel extends AbstractModel<Rayon>
+{
 
 
 
-    @Override
-    public List<Rayon> getAll()
-    {
-
-        return rayons;
-    }
-
-    @Override
-    public Rayon read(Rayon o) {
-        if(o == null)
-        {
-            return null;
-        }
-        for(Rayon i : rayons)
-        {
-            if(i.equals(o))
-            {
-                return i;
-            }
-        }
-        return null;
-    }
-
-    public Rayon add(Rayon nl)
-    {
-        if (rayons.contains(nl)) return null;
-        rayons.add(nl);
-        return nl;
-    }
 }
