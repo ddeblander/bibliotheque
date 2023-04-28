b2cba539c98b3a9cf0606a82bea7dd367b84ff16
ddeblander <david.deblander@condorcet.be>
2023-04-28 12:21:56 +0200

    -implements SpecialRayon

--- src/model/RayonModel.java
+++ src/model/RayonModel.java
@@ -1,11 +1,17 @@
 package model;
 
+import metier.Exemplaire;
 import metier.Rayon;
 
+import java.util.List;
 
-public class RayonModel extends AbstractModel<Rayon>
+
+public class RayonModel extends AbstractModel<Rayon> implements SpecialRayon
 {
 
 
-
+    @Override
+    public List<Exemplaire> listerExemplaires(Rayon r) {
+        return r.getLex();
+    }
 }
